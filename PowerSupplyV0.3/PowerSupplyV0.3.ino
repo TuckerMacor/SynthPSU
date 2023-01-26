0bb467c202f3d4f541660d10c476482b74af21af
TuckerMacor <tucker@rocam97.com>
2023-01-25 21:33:09 -0800

    Update PowerSupplyV0.3.ino

--- PowerSupplyV0.3/PowerSupplyV0.3.ino
+++ PowerSupplyV0.3/PowerSupplyV0.3.ino
@@ -1,6 +1,6 @@
 //Synth Power Supply by Tucker Macor
-//Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
-//https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+//
 //
 //
 //
